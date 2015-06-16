209b84242b202c7a0eb2b8db5de0bc44e9b54231
MCP 2015 <mcp2014@pool-mac-07.bit.uni-bonn.de>
2015-06-16 14:47:41 +0200

    Initial Commit

---
+++ MCP_2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MCP_2015
+//
+//  Created by MCP 2015 on 16/06/15.
+//  Copyright (c) 2015 MCP 2015. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
